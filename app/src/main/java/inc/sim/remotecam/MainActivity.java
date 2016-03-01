c92d56159c144cc80b2855bf471961ffdad34b52
Nil Késede <nil@ksde.pw>
2016-03-01 11:05:55 -0300

    Add Crashlytics

--- app/src/main/java/inc/sim/remotecam/MainActivity.java
+++ app/src/main/java/inc/sim/remotecam/MainActivity.java
@@ -2,12 +2,15 @@ package inc.sim.remotecam;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 
